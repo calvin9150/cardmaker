47245bddd5d1dfcc06c4efab779b90f35db9873b
calvin9150 <wonbae9150@gmail.com>
2021-05-07 05:34:36 +0900

    Add editor and preview layout

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import styles from "./login.module.css";
+import { useHistory } from "react-router";
 
 const Login = ({ authService }) => {
-  const onLogin = (e) => {
-    authService.login(e.currentTarget.textContent).then(console.log);
+  const histroy = useHistory();
+  const goToMaker = (userId) => {
+    histroy.push({
+      pathname: "/maker",
+      state: { id: userId },
+    });
   };
+  const onLogin = (e) => {
+    authService //
+      .login(e.currentTarget.textContent)
+      .then((data) => goToMaker(data.user.uid));
+  };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      user && goToMaker(user.uid);
+    });
+  });
+
   return (
     <section className={styles.login}>
       <Header />
